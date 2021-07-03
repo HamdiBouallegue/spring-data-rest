dafab596467d9b401de08a9b5a92a3bcdaf32d16
Hamdi Bouallegue <hbouallegue@vermeg.com>
2021-07-03 10:32:10 +0100

    initial commit

---
+++ src/main/java/com/hamdibouallegue/datarestdemo/DataRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hamdibouallegue.datarestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRestDemoApplication.class, args);
+	}
+
+}
